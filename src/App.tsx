2eed785a7528525333bcda1bffcd6ca97471998a
Anderson fernandes <pires.fernandescg@yahoo.com.br>
2026-09-03 20:44:06 -0300

    adiciona tela de cadastro de mesa e componentes relacionados

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ import Mesas from "./pages/Mesas/Mesas";
 import PedidosItems from "./pages/PedidosItems/PedidosItems";
 import Pedidos from "./pages/Pedidos/Pedidos";
 import Chefs from "./pages/Chefs/Chefs";
+import CadastroMesa from "./pages/CadastroMesa/CadastroMesa";
 
 function App() {
   const location = useLocation();
 
   return (
     <>
-      {/* !telasNaoMenu.includes(location.pathname) && <Menu /> */}
       {location.pathname !== "/" && <Menu />}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/mesas" element={<Mesas />} />
+        <Route path="/mesas/nova" element={<CadastroMesa />} />
         <Route path="/pedido-items/:id" element={<PedidosItems />} />
         <Route path="/pedidos" element={<Pedidos />} />
         <Route path="/chefes" element={<Chefs />} />
